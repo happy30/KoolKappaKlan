762b65ac683ea3163af2965fe861def91edafc1b
Alieke <aliekevanderlelie@hotmail.com>
2016-12-01 15:30:26 +0100

    Planning update and personal scene

---
+++ HanzakiProject/Assets/Scripts/UI/KartUIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KartUIManager : MonoBehaviour {
+
+    public Image katanaIcon;
+    public Image bombIcon;
+    public Image hookIcon;
+    public Image boxIcon;
+    public Image shurikenIcon;
+
+    public Image heldItem;
+    public Image emptyItem;
+
+    public Text player1Place;
+    public Text player2Place;
+
+    void DrawMinimap()
+    {
+
+    }
+
+    void DrawRanks()
+    {
+
+    }
+
+    void DrawSpeed()
+    {
+
+    }
+
+    void DrawHeldItem(Image itemGot)
+    {
+        heldItem = itemGot;
+    }
+
+    void HeldItemUsed()
+    {
+        heldItem = emptyItem;
+    }
+}
