d8f85c2405c9594135b6ceba989b28b218a79102
Gaming_Laptop\Enra <arnevanommen@outlook.com>
2016-12-13 14:47:16 +0100

    Cave and animations

--- HanzakiProject/Assets/_KappaKart/Scripts/KartShuriken.cs
+++ HanzakiProject/Assets/_KappaKart/Scripts/KartShuriken.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
+﻿//Made by Arne
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KartShuriken : MonoBehaviour {
 
+    public int shurikenCount;
+
+    public GameObject shurikenObject;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        Move();
+        if(Input.GetKey(InputManager.Shuriken))
+        {
+            
+        }
 	}
+    public void Move ()
+    {
+
+    }
 }
